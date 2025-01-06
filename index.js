88ad5ef3aa565cc27caf069de01213f5fe7cb7b4
michae1bo <159921466+michae1bo@users.noreply.github.com>
2025-01-06 17:32:27 +0100

    fix: always create exercise log

--- index.js
+++ index.js
@@ -40,41 +40,40 @@ app.get('/api/users', async function(req, res) {
 app.post('/api/users/:_id/exercises', async function (req, res) {
   const description = req.body.description;
   const duration = parseInt(req.body.duration);
-  const date = (req.body.date !== "") ? new Date(req.body.date) : new Date();
+  let date = (req.body.date !== "") ? new Date(req.body.date) : new Date();
   const dateString = date.toDateString();
   if (dateString === "Invalid Date") {
-    res.json({error: "Invalid Date"});
+    date = new Date();
+  } 
+  const user = await ExerciseTracker.findById(req.params._id);
+  if (user === null) {
+    res.json({error: "No user with that id"});
   } else {
-    const user = await ExerciseTracker.findById(req.params._id);
-    if (user === null) {
-      res.json({error: "No user with that id"});
-    } else {
-      user.log.push({description: description, duration: duration, date: date});
-      user.count = user.log.length;
-      await user.save();
-      res.json({
-        _id: user._id,
-        username: user.username,
-        date: dateString,
-        duration: duration,
-        description: description
-      });
-    }
+    user.log.push({description: description, duration: duration, date: date});
+    user.count = user.log.length;
+    await user.save();
+    res.json({
+      _id: user._id,
+      username: user.username,
+      date: dateString,
+      duration: duration,       description: description
+    });
   }
 });
 
 app.get('/api/users/:_id/logs', async function (req, res) {
-  const user = await ExerciseTracker.findById(req.params._id, {__v: 0});
-  if (user === null) {
-    res.json({error: "No user with that id"});
-  } else {
-    const jsonResponse = {_id: user._id, count: user.count, log: []};
+  try {
+    const user = await ExerciseTracker.findById(req.params._id, {__v: 0});
+    const jsonResponse = {count: user.count, log: []};
     let logs = []
+
     for(let i = 0; i < user.log.length; i++) {
-      const currentLog = {description: user.log[i].description, duratron: user.log[i].duration, date: user.log[i].date};
+      const currentLog = {description: user.log[i].description, duration: user.log[i].duration, date: user.log[i].date};
       logs.push(currentLog);
     }
+
     logs.sort((a, b) => a.date.getTime() - b.date.getTime());
+
     if (req.query.from !== undefined) {
       const fromDate = new Date(req.query.from);
       if (fromDate.toDateString() !== "Invalid Date") {
@@ -86,6 +85,7 @@ app.get('/api/users/:_id/logs', async function (req, res) {
         }
       }
     }
+
     if (req.query.to !== undefined) {
       const toDate = new Date(req.query.to);
       if (toDate.toDateString() !== "Invalid Date") {
@@ -99,17 +99,25 @@ app.get('/api/users/:_id/logs', async function (req, res) {
         }
       }
     }
+
     if (req.query.limit !== undefined) {
       if (!isNaN(req.query.limit)) {        
         logs = logs.slice(0, parseInt(req.query.limit));
       }
     }
+
     for (let i = 0; i < logs.length; i++) {
       logs[i].date = logs[i].date.toDateString();
     }
+
     jsonResponse.log = logs;
     res.json(jsonResponse);
+  } catch {
+    res.json({count: 0, log: []});
   }
+  
+
+    
 })
 
 const listener = app.listen(process.env.PORT || 3000, () => {
